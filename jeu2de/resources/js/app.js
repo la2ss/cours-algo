f585da62f13baf684bccf8a2cba0ad4a0d3b17ae
la2ss <69790477+la2ss@users.noreply.github.com>
2020-09-28 13:17:55 +0200

    jeu2de
    updatejeu2de

---
+++ jeu2de/resources/js/app.js
@@ -0,0 +1,86 @@
+
+let scoresGlobals = [0, 0];
+let scoreEnCours = 0;
+let joueurActif = 1;
+let jouer = true;
+
+
+let btnLancez2d = document.querySelector( ".btn-lancer");
+let btnCommuter = document.querySelector( ".btn-commuter");
+let btnNouveau = document.querySelector( ".btn-nouveau");
+
+
+initiliseLeJeu();
+
+
+let btnLancezLeDe = document.querySelector(`.btn-lancer`);
+
+btnLancezLeDe.addEventListener('click', () => {
+ if (jouer) {
+ 
+ let de = Math.floor(Math.random() * 6) + 1;
+
+ let domDe = document.querySelector('.de');
+ domDe.style.display = 'block';
+ domDe.src = `resources/images/de-${de}.png`;
+ 
+ if (de !== 1) {
+ scoreEnCours += de;
+ document.getElementById(`encours-${joueurActif}`).textContent = scoreEnCours;
+ } else {
+ joueurSuivant();
+ }
+ }
+});
+
+let btnCommutez = document.querySelector('.btn-commuter');
+btnCommutez.addEventListener('click', () => {
+ if(jouer){
+ scoresGlobals[joueurActif - 1] += scoreEnCours;
+ document.getElementById(`score-${joueurActif}`).textContent = scoresGlobals[joueurActif - 1];
+
+ if (scoresGlobals[joueurActif - 1] >= 100) {
+ document.getElementById(`nom-${joueurActif}`).textContent = 'Bravo !!!';
+ document.querySelector('.de').style.display = 'none';
+ document.querySelector(`.joueur-${joueurActif}-panel`).classList.add('vainqueur');
+ document.querySelector(`.joueur-${joueurActif}-panel`).classList.remove('active');
+ jouer = false;
+ } else {
+ joueurSuivant();
+ }
+ }
+});
+
+function joueurSuivant() {
+    joueurActif === 1 ? joueurActif = 2 : joueurActif = 1;
+    scoreEnCours = 0;
+    document.getElementById(`encours-${joueurActif}`).textContent = '0';
+    document.querySelector('.joueur-1-panel').classList.toggle('active');
+    document.querySelector('.joueur-2-panel').classList.toggle('active');
+    
+   }
+
+
+let btnNouveauJeu = document.querySelector('.btn-nouveau');
+btnNouveauJeu.addEventListener('click',() => { 
+ initiliseLeJeu();
+});
+
+
+   function initiliseLeJeu() {
+    scoresGlobals = [0, 0];
+    scoreEnCours = 0;
+    joueurActif = 1;
+    document.querySelector('.de').style.display = 'none';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('score-2').textContent = '0';
+    document.getElementById('encours-1').textContent = '0';
+    document.getElementById('encours-2').textContent = '0';
+    document.getElementById('nom-1').textContent = 'Joueur 1';
+    document.getElementById('nom-2').textContent = 'Joueur 2';
+    document.querySelector('.joueur-1-panel').classList.remove('vainqueur');
+    document.querySelector('.joueur-2-panel').classList.remove('vainqueur');
+    document.querySelector('.joueur-2-panel').classList.remove('active');
+    document.querySelector('.joueur-1-panel').classList.add('active');
+   }
+   
